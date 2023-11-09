0d4cc66cf40d5319adab255a539946dc83926ef3
daniel31x13 <daniel31x13@gmail.com>
2023-11-09 15:37:08 -0500

    improved animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className="fade-in">
       <Navbar />
       <Showcase />
       <Features />
