ba257acaca11c413632c3086dbdc04afeb012353
daniel31x13 <daniel31x13@gmail.com>
2025-07-16 18:35:57 -0400

    add twitter meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,17 @@ export default function App({ Component, pageProps }: AppProps) {
           property="og:image"
           content="https://linkwarden.app/og-image.jpg"
         />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@linkwarden_hq" />
+        <meta name="twitter:title" content="Linkwarden - Bookmarks, Evolved" />
+        <meta
+          name="twitter:description"
+          content="Linkwarden helps you collect, read, annotate, and fully preserve what matters, all in one place."
+        />
+        <meta
+          name="twitter:image"
+          content="https://linkwarden.app/og-image.jpg"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
