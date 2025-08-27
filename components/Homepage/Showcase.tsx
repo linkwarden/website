fb0bcbcd269b10d7959553b8e7979ca4003f05cd
daniel31x13 <daniel31x13@gmail.com>
2025-08-27 11:22:12 -0400

    v2.12 announcement

--- components/Homepage/Showcase.tsx
+++ components/Homepage/Showcase.tsx
@@ -57,13 +57,13 @@ export default function Showcase() {
         <div className="z-10 max-w-5xl px-5 w-fit mx-auto xl:pt-52 lg:pt-40 md:pt-32 pt-24">
           <div className="w-fit mx-auto mb-5">
             <Link
-              href="https://blog.linkwarden.app/releases/2.11"
+              href="https://blog.linkwarden.app/releases/2.12"
               target="_blank"
               className="rounded-full"
             >
               <div className="border bg-opacity-80 border-[#07334F] hover:border-[#20618a] bg-background duration-200 w-fit py-2 px-3 text-xs rounded-full relative z-10">
                 🎉️ Announcing{" "}
-                <span className="font-bold">Linkwarden 2.11</span>! 🚀
+                <span className="font-bold">Linkwarden 2.12</span>! 🚀
               </div>
             </Link>
           </div>
