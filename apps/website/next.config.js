a095d0a27c4100219b506b84ecb87061a23b2438
daniel31x13 <daniel31x13@gmail.com>
2025-12-02 13:57:32 -0500

    minor fix

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const basePath = process.env.NODE_ENV === "production" ? "/website" : "";
+const isProd = process.env.NODE_ENV === "production";
+
+const basePath = isProd ? "/website" : "";
 
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   basePath,
-  assetPrefix: basePath,
+  assetPrefix: isProd ? "/website/" : undefined,
   images: {
     unoptimized: true,
   },
